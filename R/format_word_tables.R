56a2eede1eb95705306e41bceef9e0c2cc2adf9c
Hao Ye <lhopitalified@gmail.com>
2022-08-23 12:03:43 -0400

    use R folder for source

---
+++ R/format_word_tables.R
@@ -0,0 +1,79 @@
+format_effort_perc <- function(df)
+{
+    dat_effort <- df
+    m <- NROW(dat_effort)
+    n <- NCOL(dat_effort)
+    colnames(dat_effort) <- seq(n)
+
+    dat_effort %>%
+        flextable() %>%
+        set_table_properties(layout = "autofit") %>%
+        delete_part(part = "header") %>%
+        add_header_row(values = dept_name, colwidths = n) %>%
+        theme_box() %>%
+        bg(i = 1, bg = "#D9D9D9", part = "header") %>%
+        bg(j = c(1, n), bg = "#F2F2F2") %>%
+        italic(i = 2:m, j = 1) %>%
+        bold(i = 2:m, j = 1) %>%
+        bold(i = 1, j = n) %>%
+        align(align = "right", i = 2:m) %>%
+        align(align = "center", part = "header") %>%
+        fontsize(size = 9) %>%
+        fontsize(size = 10, part = "header")
+}
+
+#'
+#' Assumes df is extracted directly from the downloaded template. The first row
+#' should have the headers, but padded with extra NA values, since the
+#' originating table has header cells that span multiple columns.
+#'
+format_teaching_summary <- function(df)
+{
+    dat_teaching_summary <- df[-1,]
+    m <- NROW(dat_teaching_summary)
+    n <- NCOL(dat_teaching_summary)
+    colnames(dat_teaching_summary) <- seq(n)
+
+    header_vals <- gsub("Reqyes/no", "Req\nyes/no", df[1,])
+    header_vals <- header_vals[!is.na(header_vals)]
+
+    dat_teaching_summary %>%
+        flextable() %>%
+        set_table_properties(layout = "autofit") %>%
+        delete_part(part = "header") %>%
+        add_header_row(values = header_vals,
+                       colwidths = c(rep.int(1, length(header_vals) - 3),
+                                     rep.int(2, 3))
+        ) %>%
+        theme_box() %>%
+        bold(i = 1) %>%
+        align(align = "right", i = 2:m, j = c(3, 5:10)) %>%
+        align(align = "center", i = 1) %>%
+        align(align = "center", part = "header") %>%
+        fontsize(size = 9) %>%
+        fontsize(size = 10, part = "header")
+}
+
+format_teaching_eval <- function(df)
+{
+    dat_teaching_evals <- df[c(-1, -2, -3),]
+    m <- NROW(dat_teaching_evals)
+    n <- NCOL(dat_teaching_evals)
+    colnames(dat_teaching_evals) <- df[3,]
+
+    dat_teaching_evals %>%
+        flextable() %>%
+        set_table_properties(layout = "autofit") %>%
+        add_header_row(values = df[2,1], colwidths = n) %>%
+        add_header_row(values = df[1,1], colwidths = n) %>%
+        theme_box() %>%
+        align(align = "right", i = 1:m, j = 2:n) %>%
+        align(align = "center", i = 3, part = "header") %>%
+        color(color = "#FFFFFF", part = "header") %>%
+        bg(bg = "#9D9D9D", part = "header") %>%
+        italic(i = 2, part = "header") %>%
+        fontsize(size = 9) %>%
+        fontsize(size = 9, i = 3, part = "header") %>%
+        fontsize(size = 10, i = c(1, 2), part = "header")
+}
+
--- R/read_rtf_table.R
+++ R/read_rtf_table.R
@@ -4,14 +4,15 @@ library(docxtractr)
 doc <- read_docx("UF_PT_PACKET.docx")
 docx_tbl_count(doc)
 docx_describe_tbls(doc)
+
 effort_perc <- docx_extract_tbl(doc, 2)
 saveRDS(effort_perc, "data/effort_perc.RDS")
 
 summary_header <- head(names(docx_extract_tbl(doc, 5)), -3)
-teaching_summary <- docx_extract_tbl(doc, 5, header = FALSE)[-1,]
+teaching_summary <- docx_extract_tbl(doc, 5, header = FALSE)
 saveRDS(teaching_summary, "data/teaching_summary.RDS")
 
-teaching_evals <- docx_extract_tbl(doc, 6)
+teaching_evals <- docx_extract_tbl(doc, 6, header = FALSE)
 saveRDS(teaching_evals, "data/teaching_evals.RDS")
 
 funding_internal <- docx_extract_tbl(doc, 22)
