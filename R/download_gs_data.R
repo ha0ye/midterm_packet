5cac2148f876dfd6fde31bbfb86e46eb82191993
Hao Ye <lhopitalified@gmail.com>
2022-10-12 11:39:09 -0400

    reorganize formatting and data acquisition code check preprint and other publication data

--- R/download_gs_data.R
+++ R/download_gs_data.R
@@ -1,6 +1,10 @@
 
 out_of_date <- function(filename, max_age = lubridate::make_difftime(day = 1))
 {
+    if (!file.exists(filename))
+    {
+        return(TRUE)
+    }
     last_updated <- fs::file_info(filename)$modification_time
     age <- Sys.time() - last_updated
     age > max_age
@@ -11,4 +15,16 @@ if (out_of_date("data/research.RDS"))
     dat_research <- get_gs_data(data_gs_url, sheet = "research",
                                 report_start_date, report_end_date)
     saveRDS(dat_research, "data/research.RDS")
+}
+
+if (out_of_date("data/degrees.RDS"))
+{
+    dat_degrees <- get_gs_data(data_gs_url, sheet = "degrees")
+    saveRDS(dat_degrees, "data/degrees.RDS")
+}
+
+if (out_of_date("data/jobs.RDS"))
+{
+    dat_jobs <- get_gs_data(data_gs_url, sheet = "jobs")
+    saveRDS(dat_jobs, "data/jobs.RDS")
 }
--- R/format_word_tables.R
+++ R/format_word_tables.R
@@ -93,3 +93,13 @@ remove_empty_rows <- function(df)
     df %>%
         filter(!if_all(everything(), function(x) {x == ""}))
 }
+
+format_jobs <- function(df)
+{
+    to_format <- df %>%
+        dplyr::mutate(date = lubridate::parse_date_time(.data$date_start, "my"),
+                      date_end = tidyr::replace_na(date_end, "present"),
+                      Dates = glue::glue("{date_start} - {date_end}")) %>%
+        dplyr::arrange(dplyr::desc(.data$date)) %>%
+        dplyr::select(Institution, Position, Dates)
+}
